8795d0e7c2ebc0cb962073281d1c7a4aa3d2b95d
Захар <zohanimaa@gmail.com>
2026-04-01 20:54:32 +0300

    update js

---
+++ src/js/1-timer.js
@@ -0,0 +1,16 @@
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+    const day = hour * 24;
+    const days = Math.floor(ms / day);
+    const hours = Math.floor((ms % day) / hour);
+    const minutes = Math.floor(((ms % day) % hour) / minute);
+    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+     return { days, hours, minutes, seconds };
+}
+    
